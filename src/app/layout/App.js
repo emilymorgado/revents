63562126678fa04330d4cac7392805ae121a8118
Emily Morgado <emilyannemorgado@gmail.com>
2018-10-16 22:11:20 -0700

    Add content to the left and right columns. Create a navbar with buttons.

--- src/app/layout/App.js
+++ src/app/layout/App.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
-import { Button } from 'semantic-ui-react';
+import { Container } from 'semantic-ui-react';
+import EventDashboard from '../../features/event/EventDashboard/EventDashboard';
+import NavBar from '../../features/nav/NavBar/NavBar';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Re-vents</h1>
-        <button className='ui icon button'>
-          <i className='smile icon'></i>
-          CSS button
-        </button>
-        <Button icon='smile' content='React Button' />
+        <NavBar />
+        <Container className='main'>
+          <EventDashboard />
+        </Container>
       </div>
     );
   }
